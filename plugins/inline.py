88750bf324ddea6b9363e5a2ae212273c83af2e6
HARLEY QUINN <88760900+harleyquinn099@users.noreply.github.com>
2021-10-03 08:19:45 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('🔘JOIN IN OUR CHANNEL🔘', url='https://t.me/subin_works/122')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
